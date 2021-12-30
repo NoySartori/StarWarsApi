e8eb9e72d482c97b6def21afc1148f03d0c812fc
NoySartori <noysartory@gmail.com>
2021-12-30 22:54:44 +0200

    working chart

--- src/Element.js
+++ src/Element.js
@@ -1,29 +1,45 @@
+import React from "react"
 
-export const Chart = ({ children, width, height }) => (
-    <svg
-        viewBox={`0 0 ${width} ${height}`}
-        width={width}
-        height={height}
-    >
-        {children}
-    </svg>
-)
+export class ChartComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-export const Bar = ({ x, y, width, height, planetName, planetPopulationSum }) => (
-    <>
-        <text>${planetPopulationSum}</text>
-        <rect
-            x={x}
-            y={y}
-            width={width}
-            height={height}
-            fill={'#D3D3D3'}
-        />
-        <text>${planetName}</text>
-    </>
-)
+    render() {
+        return (
+            <div style={{ transform: 'rotate(-90deg)', width: '250px', transformOrigin: '100% 50%' }}>
+                {this.props.planetsAndPopulations.map(planetAndPopulation => {
+                    return (
+                        <BarComponent name={planetAndPopulation.name} number={planetAndPopulation.number} heightPercentage={planetAndPopulation.number / this.props.maxPopulation}></BarComponent>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+const barTexts = {
+    transform: 'rotate(90deg)',
+    display: 'inline-block',
+    // marginTop: '50px',
+    marginBottom: '50px',
+    width: '50px'
+};
+
+export class BarComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
 
 
-
-
-
+        return (
+            <div>
+                <div style={barTexts}>{this.props.name}</div>
+                <div style={{ display: 'inline-block', height: '30px', width: this.props.heightPercentage * 0.2 * 100 + "%", backgroundColor: 'blue' }}>
+                </div>
+                <div style={barTexts}>{this.props.number}</div>
+            </div>
+        )
+    }
+}
--- src/Pages/ChartBarComponent.js
+++ src/Pages/ChartBarComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SwagiApiService } from '../SwagiApiService';
-import { Chart, Bar } from '../Element';
+import { ChartComponent, BarComponent } from '../Element';
 
 export class ChartBarComponent extends React.Component {
     constructor(props) {
@@ -28,36 +28,10 @@ export class ChartBarComponent extends React.Component {
         } else if (!isLoaded) {
             return <div>ChartBar...</div>;
         } else {
-            const barWidth = 50
-            const barMargin = 30;
-
             const maxPopulation = Math.max(...planetsAndPopulationData.map(planetAndPopulation => planetAndPopulation.number));
-            const chartHeight = maxPopulation + 20;
-            const chartWidth = planetsAndPopulationData.length * (barWidth + barMargin);
 
             return (
-                <>
-
-                    <Chart height={chartHeight} width={chartWidth}>
-                        {planetsAndPopulationData.map((planetAndPopulation, index) => {
-
-                            const barHeight = planetAndPopulation.number;
-
-                            return (
-                                <Bar
-                                    key={planetAndPopulation.name}
-                                    x={index * (barWidth + barMargin)}
-                                    y={chartHeight - barHeight}
-                                    width={barWidth}
-                                    height={barHeight}
-                                    planetName={planetAndPopulation.name}
-                                    planetPopulationSum={planetAndPopulation.number}
-                                />
-                            );
-                        })}
-                    </Chart>
-                </>
-
+                <ChartComponent planetsAndPopulations={planetsAndPopulationData} maxPopulation={maxPopulation}></ChartComponent>
             );
         }
     }
